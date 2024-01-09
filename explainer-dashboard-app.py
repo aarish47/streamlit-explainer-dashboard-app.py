e9ae5e0196cc2d1096961995e9ebe644b145f889
Aarish Khan <154256796+aarish47@users.noreply.github.com>
2024-01-10 00:21:54 +0500

    Update explainer-dashboard-app.py

--- explainer-dashboard-app.py
+++ explainer-dashboard-app.py
@@ -1,17 +1,18 @@
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from explainerdashboard.data import titanic_data, titanic_labels
 from explainerdashboard import ClassifierExplainer, ExplainerDashboard
-from explainerdashboard.datasets import titanic_survive, features_descriptions
 
-# train test split
-x_train, y_train, x_test, y_test = titanic_survive()
+# Create a train-test split
+x_train, x_test, y_train, y_test = train_test_split(
+    titanic_data.values, titanic_labels, test_size=0.2, random_state=42
+)
 
-# creating a model
+# Replace RandomForestClassifier with the actual model instantiation and training
 model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
 model.fit(x_train, y_train)
 
-explainer = ClassifierExplainer(model, x_test, y_test,
-                                cats=['Sex', 'Deck', 'Embarked'],
-                                descriptions=features_descriptions,
-                                labels=['Not survived', 'Survived'])
+# Replace 'your_model' with the actual model you're using
+explainer = ClassifierExplainer(model, x_test, y_test)
 
+# Run the Explainer Dashboard
 ExplainerDashboard(explainer).run()
